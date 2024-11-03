e4e84eba55e4ad0104acdef7b491af70e33c52ac
Biiel25 <gabrielsantos@ucl.br>
2024-11-03 15:25:32 -0300

    Config-Gemini

--- src/WebCrawler.py
+++ src/WebCrawler.py
@@ -1,6 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+    text = text.replace("•", "  *")
+    return Markdown(textwrap.indent(text, "> ", predicate=lambda _: True))
+
+    # Used to securely store your API key
+from google.colab import userdata
+genai.configure(api_key='AIzaSyDUnXqUIVReCj1do3tAKqAQ3P-PPRSTcLU')
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 # Scrape do site do gov pesquisando sobre noticias sobre vagas
 def scrape_es_government():
     url = 'https://www.es.gov.br/Noticias'
